b95be54e10ba4907ac9a2f8d0366ef8f849bfb64
sarahwambui96 <sarahwambui843@gmail.com>
2023-03-09 19:51:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+// whiteCards
+//const names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(names, event){
+    const messages = [];
+    for (let i = 0;
+        i < names.length;
+        i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+        }
+        return messages;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+// countDown()
+let number = 10;
+function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number-=1;
+    }
+    console.log(number);
+}
